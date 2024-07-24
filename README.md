5e1df881362a55066a4a477201589dfaecbf725e
khavelemarline <khavelemarline@gmail.com>
2024-07-24 18:02:03 +0200

    Added documentation and tests

---
+++ README.md
@@ -0,0 +1,56 @@
+### Glucose Level API 
+Glucose API that stores users glucose values and perform basic CRUD functionalities
+
+Built with:
+```shell
+Python version 3
+Django
+Djangorestframework
+```
+#### Setting up the project
+
+- Clone the project using git clone from Github: ```https://github.com/Marlinekhavele/UnaHealth```
+- Enter the project directory i.e ```cd app```
+- create virtual environment
+```shell
+virtualenv env -p python3.11 - use your local python
+```
+*Activate the virtual env with the below command
+```shell
+source env/bin/activate
+```
+
+##### Install deps:
+```shell
+pip install -r requirements.txt
+```
+##### To check django admin you need to create a user using the below command
+- python manage.py createsuperuser
+```shell
+```
+##### Ensure Database is on the same level
+```
+python manage.py makemigrations
+python manage.py migrate
+```
+##### Run the project Locally with the below command 
+```shell
+python manage.py runserver
+```
+
+
+##### Running Tests Locally
+```shell
+python manage.py test
+
+```
+some environment variables you will use inside your `.env`file just copy this.
+```shell
+DB_USER=postgres
+DB_HOST=localhost
+DB_NAME=una_health_app
+DB_PASSWORD=password
+```
+service has two DB services:
+- `una_health_app_db` bound to port 5432 - a DB for local testing,
+- `una_health_app_test_db` bound to port 5433 - a DB for running tests.
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from datetime import datetime
+from user.models import User
+from .models import GlucoseLevel
 
 # Create your tests here.
+
+class GlucoseLevelTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='marline', password='password')
+        
+        self.glucose_level = GlucoseLevel.objects.create(
+            user=self.user,
+            timestamp=datetime.now(),
+            glucose_value=120
+        )
+        self.glucose_level_url = reverse('glucose-level-detail', kwargs={'pk': self.glucose_level.id})
+        self.glucose_level_list_url = reverse('glucose-level-list')
+        self.glucose_level_create_url = reverse('glucose-level-create')
+        
+    def test_create_glucose_level(self):
+        data = {
+            'user': self.user.id,
+            'timestamp': '2024-07-24T10:00:00Z',
+            'glucose_value': 130
+        }
+        response = self.client.post(self.glucose_level_create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(GlucoseLevel.objects.count(), 2)
+        self.assertEqual(GlucoseLevel.objects.latest('id').glucose_value, 130)
+        
+    def test_list_glucose_levels(self):
+        response = self.client.get(self.glucose_level_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)  
+
+    def test_retrieve_glucose_level(self):
+        response = self.client.get(self.glucose_level_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['glucose_value'], 120)
+
+    
